5bc4d5996fcc30ec628961a91bf0edfcbbd344de
kevinpro <3121416933@qq.com>
2019-12-24 23:52:37 +0800

    OJ test2 Accept update the file

--- DS_homework/module2.cpp
+++ DS_homework/module2.cpp
@@ -1,288 +1,297 @@
 #include<iostream>
-#include <cassert>
 using namespace std;
-
-struct Node {
-	int val = -1;
-	Node* left = NULL;
-	Node* right = NULL;
-	int time = 1;
-	int left_child_num = 0;
-	int right_child_num = 0;
+ //pass oj test
+template<class T>
+class MinHeap
+{
+public:
+    MinHeap()
+    {
+        _maxSize = 0;
+        _index = -1;
+        _minHeap = NULL;
+    }
+ 
+    MinHeap(int maxsize,int flag)
+    {
+        _maxSize = maxsize;
+        _index = -1;
+        _minHeap = new T[maxsize];
+        mode = flag;
+ 
+    }
+ 
+    MinHeap(MinHeap& h)
+    {
+        _maxSize = h._maxSize;
+        _index = h._index;
+        for (int i = 0; i < h._index; i++)
+            _minHeap[i] = h._minHeap[i];
+    }
+ 
+    ~MinHeap()
+    {
+        delete[] _minHeap;
+    }
+ 
+    T* GetMinHeap()
+    {
+        return _minHeap;
+    }
+ 
+    bool IsEmpty()
+    {
+        return _index == -1;
+    }
+    bool IsFool()
+    {
+        return _index == _maxSize;
+    }
+ 
+    bool Add(T x)
+    {
+        if (IsFool())
+            return false;
+ 
+        _index++;
+        _minHeap[_index] = x;
+        return true;
+    }
+ 
+    void CreatMinHeap()
+    {
+        if (IsEmpty())
+            return;
+ 
+        for (int i = (_index - 1) / 2; i > -1; i--)
+        {
+            AdjustDown(i);
+        }
+    }
+ 
+    void AdjustDown(int index)
+    {
+        if (IsEmpty())
+            return;
+ 
+        int temp;
+        int flag = 0;
+ 
+        while ((index * 2 + 1) <= _index && flag == 0)
+        {
+            if (mode)
+            {
+                if (_minHeap[index] > _minHeap[index * 2 + 1]) 
+                    temp = index * 2 + 1;
+                else
+                    temp = index;
+ 
+                if (index * 2 + 2 <= _index)
+                {
+                    if (_minHeap[temp] > _minHeap[index * 2 + 2])
+                        temp = index * 2 + 2;
+                }
+ 
+                if (temp != index)
+                {
+                    T t = _minHeap[temp];
+                    _minHeap[temp] = _minHeap[index];
+                    _minHeap[index] = t;
+                    index = temp;
+ 
+                }
+                else
+                    flag = 1;
+            }
+            else
+            {
+                if (_minHeap[index] < _minHeap[index * 2 + 1])
+                    temp = index * 2 + 1;
+                else
+                    temp = index;
+ 
+                if (index * 2 + 2 <= _index)
+                {
+                    if (_minHeap[temp] < _minHeap[index * 2 + 2])
+                        temp = index * 2 + 2;
+                }
+ 
+                if (temp != index)
+                {
+                    T t = _minHeap[temp];
+                    _minHeap[temp] = _minHeap[index];
+                    _minHeap[index] = t;
+                    index = temp;
+ 
+                }
+                else
+                    flag = 1;
+            }
+ 
+        }
+ 
+    }
+ 
+    void AdjustUp(int index)
+    {
+        if (index < 0 || index == 0)
+            return;
+ 
+        int flag = 0;
+        while (index != 0 || flag == 0)
+        {
+            if (mode)
+            {
+                int i = (index - 1) / 2;
+                if (_minHeap[i] > _minHeap[index])
+                {
+                    T t = _minHeap[i];
+                    _minHeap[i] = _minHeap[index];
+                    _minHeap[index] = t;
+                }
+                else
+                    flag = 1;
+            }
+            else
+            {
+                int i = (index - 1) / 2;
+                if (_minHeap[i] < _minHeap[index])
+                {
+                    T t = _minHeap[i];
+                    _minHeap[i] = _minHeap[index];
+                    _minHeap[index] = t;
+                }
+                else
+                    flag = 1;
+            }
+ 
+            index = (index - 1) / 2;
+        }
+ 
+    }
+ 
+    bool InsertNode(const T& v)
+    {
+        if ((_index + 1) < _maxSize)
+        {
+            _index++;
+            _minHeap[_index] = v;
+            AdjustUp(_index);
+            return true;
+        }
+        else
+            return false;
+    }
+    bool DeleteNode(T& v)
+    {
+        if (IsEmpty())
+            return false;
+ 
+        v = _minHeap[0];
+        _minHeap[0] = _minHeap[_index];
+        _index--;
+ 
+        AdjustDown(0);
+ 
+    }
+ 
+     
+    void HeapSort()
+    {
+        while (_index > -1)
+        {
+ 
+            T temp = _minHeap[0];
+            _minHeap[0] = _minHeap[_index];
+            _minHeap[_index] = temp;
+ 
+            _index--;
+            AdjustDown(0);
+ 
+ 
+        }
+    }
+ 
+ 
+    T* _minHeap;
+    int         _index;
+    int         _maxSize;
+    int mode;
 };
-
-int total_num(0);
-
-//use visit function to test build function complete
-int test_visit(Node* p)
-{
-	if (p == NULL)
-	{
-		return 0;
-	}
-	else
-	{
-		test_visit(p->left);
-		cout << "testval:  " << p->val << "   testtime  " << p->time << "   testleftnum: " << p->left_child_num << "   testrightnum  " << p->right_child_num << endl;
-		test_visit(p->right);
-	}
-	return 0;
-}
-
-int test_simpler_test(Node* p)
-{
-	if (p == NULL)
-	{
-		return 0;
-	}
-	else
-	{
-		test_simpler_test(p->left);
-		for (int i(0); i < p->time; i++)
-		{
-			cout << p->val << " ";
-		}
-		test_simpler_test(p->right);
-	}
-	
-	return 0;
-}
-
-//perform bad in some case
-Node* build(int p, Node* q)
-{
-	if (q == NULL)
-	{
-		assert(0);//Hit bad trap here
-	}
-	if (q->val == p)
-	{
-		q->time++;
-		return q;
-	}
-	if (q->val > p)
-	{
-		q->left_child_num++;
-		if (q->left == NULL)
-		{
-			Node* temp = new Node;
-			temp->time = 1;
-			temp->left_child_num = 0;
-			temp->right_child_num = 0;
-			temp->val = p;
-			q->left = temp;
-			return NULL;
-		}
-		else
-		{
-			build(p, q->left);
-		}
-	}
-	else
-	{
-		q->right_child_num++;
-		if (q->right == NULL)
-		{
-			Node* temp = new Node;
-			temp->time = 1;
-			temp->left_child_num = 0;
-			temp->right_child_num = 0;
-			temp->val = p;
-			q->right = temp;
-			return NULL;
-		}
-		else
-		{
-			build(p, q->right);
-		}
-	}
-	return NULL;
-}
-
-
-Node* build2(int p, Node* q)
-{
-	if (q == NULL)
-	{
-		assert(0);//Hit bad trap here
-	}
-	//if (q->val == p)
-	//{
-	//	q->time++;
-	///	return q;
-	//}
-	if (q->val >= p)
-	{
-		q->left_child_num++;
-		if (q->left == NULL)
-		{
-			Node* temp = new Node;
-			temp->time = 1;
-			temp->left_child_num = 0;
-			temp->right_child_num = 0;
-			temp->val = p;
-			q->left = temp;
-			return NULL;
-		}
-		else
-		{
-			build(p, q->left);
-		}
-	}
-	else
-	{
-		q->right_child_num++;
-		if (q->right == NULL)
-		{
-			Node* temp = new Node;
-			temp->time = 1;
-			temp->left_child_num = 0;
-			temp->right_child_num = 0;
-			temp->val = p;
-			q->right = temp;
-			return NULL;
-		}
-		else
-		{
-			build(p, q->right);
-		}
-	}
-	return NULL;
-}
-
-int already_travel = 0;
-/*
-int get_min_num(Node* p)
-{
-	if (p == NULL)
-		return 0;
-	while (p->left != NULL)
-	{
-		p = p->left;
-	}
-	return p->val;
-}
-
-int get_mid_num(Node* p)
-{
-	if (p == NULL)
-		return 0;
-	if (total_num % 2 == 1&&total_num+1/2>=already_travel&& total_num + 1 / 2<=already_travel+p->time)
-	{
-		return p->val;
-	}
-	if (total_num % 2 == 0 )
-	{
-		if (total_num / 2 >= already_travel && total_num / 2 <= already_travel + p->time)
-		{
-			if (p->time == 1)
-			{
-				int result = p->val + get_min_num(p->left);
-				return result / 2;
-			}
-			else
-			{
-				return p->val;
-			}
-		}
-	}
-	if (p->left == NULL)
-	{
-		already_travel++;
-		return get_mid_num(p->right);
-	}
-	if (p->right == NULL)
-	{
-		already_travel++;
-		return get_mid_num(p->left);
-	}
-	if (p->left_child_num + already_travel > (total_num + 1) / 2)
-	{
-		return get_mid_num(p->left);
-	}
-	else
-	{
-		already_travel += p->left_child_num;
-		return get_mid_num(p->right);
-	}
-
-
-
-}
-*/
-int target(0);
-
-int get_num_pos(int pos, int& al, Node* p)
-{
-	if (p == NULL)
-		return 0;
-	
-	get_num_pos(pos, al, p->left);
-	al++;
-	if (pos == al)
-	{
-		target = p->val;
-		return 0;
-	}
-	get_num_pos(pos, al, p->right);
-	return 0;
-}
-
-int module2()
-{
-	int number;
-	cin >> number;
-	Node* head=NULL;
-
-	for (int i(0); i < number; i++)
-	{
-		int mydata;
-		cin >> mydata;
-		total_num++;
-		if (head == NULL)
-		{
-			Node* temp = new Node;
-			temp->val = mydata;
-			head = temp;
-			test_simpler_test(head);
-			cout << endl;
-			cout << "result  " << mydata << endl;
-		}
-		else
-		{
-			//build(mydata, head);
-			build2(mydata, head);//first test build tree module
-			//cout << "result  " << get_mid_num(head) << endl;;
-			if (total_num % 2 == 1)
-			{
-				int pos = 0;
-				get_num_pos((total_num + 1) / 2, pos, head);
-				test_simpler_test(head);
-				cout << endl;
-				cout << "resutlt: " << target << endl;
-			}
-			else
-			{
-				test_simpler_test(head);
-				cout << endl;
-				int pos = 0;
-				get_num_pos((total_num) / 2, pos, head);
-				int buf = target; 
-				pos = 0;
-				get_num_pos((total_num) / 2+1, pos, head);
-				buf += target;
-				cout << "resutlt: " << buf/2 << endl;
-			}
-
-		}
-	}
-	cout << endl;
-	//test_visit(head);
-	return 0;
-}
-
+ 
 int main()
 {
-	module2();
-	return 0;
+    MinHeap<int> min_heap(6000, 1);
+    MinHeap<int> max_heap(6000, 0);
+    min_heap.CreatMinHeap();
+    max_heap.CreatMinHeap();
+    int num;
+    cin >> num;
+    for (int i(0); i < num; i++)
+    {
+        int temp;
+        cin >> temp;
+        if (min_heap._index == -1)
+        {
+            min_heap.InsertNode(temp);
+        }
+        else
+        {
+            int* p = min_heap.GetMinHeap();
+            if (p[0] < temp)
+            {
+                min_heap.InsertNode(temp);
+            }
+            else
+            {
+                max_heap.InsertNode(temp);
+            }
+        }
+        if (min_heap._index - max_heap._index > 1)
+        {
+            min_heap.DeleteNode(temp);
+            max_heap.InsertNode(temp);
+        }
+        if (max_heap._index - min_heap._index > 1)
+        {
+            max_heap.DeleteNode(temp);
+            min_heap.InsertNode(temp);
+        }
+        /*
+        cout << "check1 ";
+        for (int i(0); i < min_heap._index+1; i++)
+        {
+            int* p = min_heap.GetMinHeap();
+            cout << p[i] << " ";
+        }
+        cout << endl;
+        cout << "check2 ";
+        for (int i(0); i < max_heap._index+1;i++)
+        {
+            int* p = max_heap.GetMinHeap();
+            cout << p[i] << " ";
+        }
+        cout << endl;
+        */
+ 
+        if ((min_heap._index + 1 + max_heap._index + 1) % 2 == 1)
+        {
+            int* p;
+            if(min_heap._index>max_heap._index)
+                 p = min_heap.GetMinHeap();
+            else
+                p = max_heap.GetMinHeap();
+            cout  << p[0] << endl;
+        }
+        else
+        {
+            int* p1 = min_heap.GetMinHeap();
+            int* p2 = max_heap.GetMinHeap();
+            cout <<  (p1[0]+p2[0])/2 << endl;
+        }
+    }
+     
+ 
+    return 0;
 }
+ 
